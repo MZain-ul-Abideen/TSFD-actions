da958a0fd878faa1bdfb58f7746e63b2ca4740f6
MZain-ul-Abideen <MZain-ul-Abideen@users.noreply.github.com>
2025-01-08 21:31:40 +0000

    deploy: 5a0336a7484f34fd7c430668876d775677774630

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './Home'; // Explicit file extension to avoid ESLint error
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
